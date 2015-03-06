8cebbf204c30752731c143d78ead2ce15e99919c
BrianMcBrayer <brandmcbrayer@live.com>
2015-03-06 16:30:23 -0500

    Added 100, 1000, 5000 multibind examples

--- src/config/routes.config.js
+++ src/config/routes.config.js
@@ -19,10 +19,34 @@
         }
       },
       {
+        url: '/multibind/multibind100',
+        config: {
+          templateUrl: '/views/multibind/multibind100.tmpl.html',
+          controller: 'multibindController100',
+          controllerAs: 'vm'
+        }
+      },
+      {
+        url: '/multibind/multibind1000',
+        config: {
+          templateUrl: '/views/multibind/multibind1000.tmpl.html',
+          controller: 'multibindController1000',
+          controllerAs: 'vm'
+        }
+      },
+      {
+        url: '/multibind/multibind5000',
+        config: {
+          templateUrl: '/views/multibind/multibind5000.tmpl.html',
+          controller: 'multibindController5000',
+          controllerAs: 'vm'
+        }
+      },
+      {
         url: '/multibind/multibind10000',
         config: {
           templateUrl: '/views/multibind/multibind10000.tmpl.html',
-          controller: 'multibindController',
+          controller: 'multibindController10000',
           controllerAs: 'vm'
         }
       }
